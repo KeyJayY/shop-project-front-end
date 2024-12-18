18308882aa5e78d6225217613d5ccc4db709b512
Krzysztof Jabłoński <krzys.jablonski2003@gmail.com>
2024-12-18 16:22:09 +0100

    add update user data function

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -10,7 +10,9 @@ import stylesUserPage from "./UserPage.module.scss"
 function UserPage() {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
-    const { isAuthenticated, loading: authLoading } = useAuth();
+    const [editing, setEditing] = useState(false);
+    const [editedData, setEditedData] = useState({});
+    const {isAuthenticated, loading: authLoading} = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -37,10 +39,26 @@ function UserPage() {
         return <>loading...</>;
 
     if (!isAuthenticated) {
-        return <Navigate to={"/"} />;
+        return <Navigate to={"/"}/>;
     }
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setEditedData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
 
+    const handleSave = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        const result = await axios.put("/api/changeUserData", editedData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
     return (
         <>
@@ -51,14 +69,26 @@ function UserPage() {
             <main className={stylesUserPage.userPanel}>
 
                 <div className={stylesUserPage.userDetails}>
-                    <p><strong>imie: </strong>{data.first_name}</p>
-                    <p><strong>nazwisko: </strong>{data.last_name}</p>
-                    <p><strong>miasto: </strong>{data.address_city}</p>
-                    <p><strong>adres: </strong>{data.address}</p>
-                    <p><strong>data urodzenia: </strong>{new Intl.DateTimeFormat('pl-PL', {day: "2-digit", month: "2-digit", year: "numeric"}).format(new Date(data.birth_date))}</p>
-                    <button className={stylesUserPage.editButton}>Edytuj</button>
+                    <p><strong>imie: </strong>{editing ? <input type={"text"} name="first_name" value={editedData.first_name} onChange={handleInputChange}/> : data.first_name}</p>
+                    <p><strong>nazwisko: </strong>{editing ? <input type={"text"} name="last_name" value={editedData.last_name} onChange={handleInputChange}/> : data.last_name}</p>
+                    <p><strong>miasto: </strong>{editing ? <input type={"text"} name="address_city" value={editedData.address_city} onChange={handleInputChange}/> : data.address_city}</p>
+                    <p><strong>adres: </strong>{editing ? <input type={"text"} name="address" value={editedData.address} onChange={handleInputChange}/> : data.address}</p>
+                    <p><strong>data urodzenia: </strong>{editing ?
+                        <input type={"date"} name="birth_date" value={new Date(editedData.birth_date).toISOString().split('T')[0]} onChange={handleInputChange}/> : new Intl.DateTimeFormat('pl-PL', {
+                            day: "2-digit",
+                            month: "2-digit",
+                            year: "numeric"
+                        }).format(new Date(data.birth_date))}</p>
+                    {editing ? <>
+                            <button className={stylesUserPage.editButton} onClick={handleSave}>Zapisz</button>
+                            <button className={stylesUserPage.editButton} onClick={() => {setEditing(false)}}>Anuluj</button>
+                        </>
+                        : <button className={stylesUserPage.editButton} onClick={() => {
+                            setEditing(!editing)
+                            setEditedData(data)
+                        }}>Edytuj</button>}
                 </div>
-                <Cart />
+                <Cart/>
             </main>
         </>
     );
